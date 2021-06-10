98c0ac08ee7ffd8ea004d2ad741f832a661abcd9
haule2020 <61819317+haule2020@users.noreply.github.com>
2021-06-10 12:31:54 +0700

    Delete cxtPolicy directory
    remove

--- cxtPolicy/src/main/java/bodyResponseFields/CoverageHistory.java
+++
@@ -1,8 +0,0 @@
-package bodyResponseFields;
-
-//POJO class to store fields converted from  body response JSON file
-public class CoverageHistory {
-	public String coverageClass;
-	public String coverageEffectiveDate;
-	public String transactionDate;
-}
